607336f1cf1e64e4675cf130dc8effb367d50e16
Ricardo Quintero <ricardo.quintero.botia@schibsted.hu>
2017-06-30 16:01:29 +0200

    Add:Greeting Kata complete

--- TDDKatas/GreetingKata/Greeting.swift
+++ TDDKatas/GreetingKata/Greeting.swift
@@ -13,63 +13,55 @@ class Greeting: NSObject {
     func greet(_ names: String?...) -> String {
 
         if names.count > 0 && names.first != nil {
-            var nameList = [String]()
-            var shoutNameList = [String]()
-            for name in names {
+            let sortedNames = sortNames(names as! [String])
 
-                if name!.contains("\"") {
-                    nameList.append(name!.replacingOccurrences(of:"\"", with:""))
-                }else if name!.contains(",") {
-                    let splittedNames = name!.components(separatedBy: ", ")
-                    for newName in splittedNames {
-                        if newName.uppercased() == newName {
-                            shoutNameList.append(newName)
-                        }else{
-                            nameList.append(newName)
-                        }
-                    }
-                } else if name!.uppercased() == name! {
-                    shoutNameList.append(name!)
-                }else{
-                    nameList.append(name!)
-                }
-            }
-
-            if !nameList.isEmpty && shoutNameList.isEmpty {
-                return normalGreeting(nameList)
-            } else if nameList.isEmpty && !shoutNameList.isEmpty {
-                return shoutGreeting(shoutNameList)
+            if !sortedNames.normalList.isEmpty && sortedNames.shoutingList.isEmpty {
+                return normalGreeting(sortedNames.normalList)
+            } else if sortedNames.normalList.isEmpty && !sortedNames.shoutingList.isEmpty {
+                return shoutGreeting(sortedNames.shoutingList)
             } else {
-                return mixedGreeting(nameList, shoutNameList)
+                return mixedGreeting(sortedNames.normalList, sortedNames.shoutingList)
             }
         }
 
         return "Hello, my friend."
     }
 
-    func normalGreeting(_ names: [String]) -> String {
-        var greeting = "Hello, "
+    func sortNames(_ names: [String]) -> (normalList: [String], shoutingList: [String]) {
+        var normalList = [String]()
+        var shoutingList = [String]()
+
         for name in names {
-            if names.count > 1 && name == names.last! {
-                greeting.append(" and \(name)")
-            }
-            else {
-                greeting.append("\(name)")
-                if names.count > 1 && name != names[names.count-2] {
-                    greeting.append(", ")
-                }
+            if name.contains("\"") {
+                normalList.append(name.replacingOccurrences(of:"\"", with:""))
+            }else if name.contains(",") {
+                let splittedNames = name.components(separatedBy: ", ")
+                let subSorted = sortNames(splittedNames)
+                normalList.append(contentsOf: subSorted.normalList)
+                shoutingList.append(contentsOf: subSorted.shoutingList)
+            } else if name.uppercased() == name {
+                shoutingList.append(name)
+            }else{
+                normalList.append(name)
             }
         }
 
-        greeting.append(".")
-        return greeting
+        return (normalList, shoutingList)
+    }
+
+    func normalGreeting(_ names: [String]) -> String {
+        return createGreeting("Hello, ", ".", " and ", names)
     }
 
     func shoutGreeting(_ names: [String]) -> String {
-        var greeting = "HELLO "
+        return createGreeting("HELLO ", "!", " AND ", names)
+    }
+
+    func createGreeting(_ start: String, _ ending: String, _ andString: String, _ names: [String]) -> String {
+        var greeting = start
         for name in names {
-            if name == names.last! && names.count > 1 {
-                greeting.append(" AND \(name)")
+            if names.count > 1 && name == names.last! {
+                greeting.append("\(andString)\(name)")
             }
             else {
                 greeting.append("\(name)")
@@ -79,9 +71,10 @@ class Greeting: NSObject {
             }
         }
 
-        greeting.append("!")
+        greeting.append(ending)
         return greeting
     }
+
 
     func mixedGreeting(_ names: [String], _ shoutingNames: [String]) -> String {
         let greeting = normalGreeting(names)
